3a3ad280f62e3fa8782da73ac841f6fdd67919d7
Gonzalo Bonfrisco <gbonfrisco@gmail.com>
2022-01-26 17:43:13 -0300

    Fin seccion 9

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,14 +5,19 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [`
+  LI{
+    cursor: pointer;
+  }
+  `  ]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSujeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
@@ -36,7 +41,20 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false;
-    // TODO
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais(termino)
+      .subscribe(
+        paises => this.paisesSujeridos = paises.splice(0, 5),
+        (err) => this.paisesSujeridos = []
+      );
+
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -10,18 +10,22 @@ export class PaisService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
+  get httpParams() {
+    return new HttpParams().set('fields', 'name,capital,cca2,flags,population');
+  }
+
   constructor(private http: HttpClient) { }
 
   buscarPais(termino: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/name/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   buscarCapital(termino: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   getPaisPorAlpha(id: string): Observable<Country[]> {
@@ -30,6 +34,12 @@ export class PaisService {
 
     const url = `${this.apiUrl}/alpha`;
     return this.http.get<Country[]>(url, { params });
+  }
+
+  buscarRegion(region: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/region/${region}`;
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
 }
